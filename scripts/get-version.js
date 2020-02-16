bf3bc9411716a7cb17d10ebe4a0c47e9abfbe7f0
Nick Ball <nicholaspball@gmail.com>
2020-02-16 14:05:40 -0800

    Try custom script

--- scripts/get-version.js
+++ scripts/get-version.js
@@ -1,1 +1,12 @@
-console.log(require("../package.json").version);
+const pkg = require("../package.json");
+
+function getVersion() {
+  return `releases-example@${pkg.version}`;
+}
+
+module.exports = getVersion;
+
+if (require.main === "module") {
+  // eslint-disable-next-line
+  console.log(getVersion());
+}
--- scripts/sentry-custom.js
+++ scripts/sentry-custom.js
@@ -1,7 +1,13 @@
+/**
+ * A quick-and-dirty custom Sentry releaser that handles parsing the Git history
+ * and calling the API in the correct format. If you can avoid it, use the
+ * Sentry/GitHub integration instead!
+ *
+ */
 /* eslint-disable no-console */
 require("dotenv").config();
 
-let pkg = require("./package.json");
+let getVersion = require("./get-version");
 let fetch = require("node-fetch");
 let SentryCli = require("@sentry/cli");
 let { promisify } = require("util");
@@ -9,122 +15,17 @@ let { promisify } = require("util");
 let exec = promisify(require("child_process").exec);
 let cli = new SentryCli();
 
-async function parseLog(from, to) {
-  let cmd = `git log `;
-
-  if (from && to) {
-    cmd += `${from}..${to} `;
-  }
-
-  cmd += "--pretty='@begin@%H\t%an\t%ae\t%aI\t%B\t' ";
-  cmd += "--name-status";
-
-  console.log("Git command: ", cmd);
-
-  let { stdout, stderr } = await exec(cmd);
-
-  if (stderr) {
-    console.error(stderr);
-    process.exit(1);
-  }
-
-  let commits = stdout.split("@begin@").slice(1);
-
-  let parsed = commits.map(commit => parseCommit(commit));
-
-  return parsed;
-}
-
-function parseCommit(text) {
-  let [id, authorName, authorEmail, timestamp, message, ...rest] = text.split(
-    "\t"
-  );
-  let files = rest
-    .join("\t")
-    .trim()
-    .split("\n");
-
-  let patchSet = [];
-  let gitTypeToSentryType = gitType => {
-    if (gitType.startsWith("R") || gitType.startsWith("C")) {
-      return "M";
-    }
-
-    return gitType;
-  };
-
-  for (let file of files) {
-    let [type, path] = file.split("\t");
-
-    patchSet.push({
-      path,
-      type: gitTypeToSentryType(type),
-    });
-  }
-
-  let payload = {
-    patch_set: patchSet,
-    respository: "npbee/sentry-releases-example",
-    author_name: authorName,
-    author_email: authorEmail,
-    timestamp,
-    message,
-    id,
-  };
-
-  return payload;
-}
-
-async function fetchLatestReleaseCommit() {
-  let releases = await fetch(
-    `https://sentry.io/api/0/organizations/npbee/releases/`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.SENTRY_AUTH_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-    }
-  ).then(resp => {
-    if (resp.ok) {
-      return resp.json();
-    } else {
-      throw new Error(resp.statusText);
-    }
-  });
-  let releaseWithCommit = releases.find(release => release.lastCommit);
-
-  if (releaseWithCommit) {
-    return releaseWithCommit.lastCommit.id;
-  }
-}
-
-async function createRelease(version, commits) {
-  let body = {
-    version,
-    projects: [process.env.SENTRY_PROJECT],
-    commits,
-  };
-
-  return await fetch(`https://sentry.io/api/0/organizations/npbee/releases/`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.SENTRY_AUTH_TOKEN}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  }).then(resp => {
-    if (resp.ok) {
-      return resp.json();
-    } else {
-      throw new Error(resp.statusText);
-    }
-  });
-}
+run().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
 
 async function run() {
-  // let { stdout } = await exec("git log --pretty='%H' -n 1");
+  let version = getVersion();
+
+  console.log("Releasing version: ", version);
+
   let latestReleaseCommit = await fetchLatestReleaseCommit();
-  let version = `releases-example@${pkg.version}`;
   let commits = await parseLog(latestReleaseCommit, "HEAD");
 
   let release = await createRelease(version, commits);
@@ -150,7 +51,122 @@ async function run() {
   console.log("Done!");
 }
 
-run().catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+/**
+ * Parse the git history into the format that Sentry expects.
+ * A specialized version of this module (thanks!):
+ *   - https://github.com/domharrington/node-gitlog
+ */
+async function parseLog(from, to) {
+  let cmd = `git log `;
+
+  if (from && to) {
+    cmd += `${from}..${to} `;
+  }
+
+  // Grab all of the fields we need from the log and delimit them so that
+  // we can grab them all later
+  cmd += "--pretty='@begin@%H\t%an\t%ae\t%aI\t%B\t' ";
+  cmd += "--name-status";
+
+  let { stdout, stderr } = await exec(cmd);
+
+  if (stderr) {
+    console.error("Error running Git command!");
+    throw new Error(stderr);
+  }
+
+  return stdout
+    .split("@begin@")
+    .slice(1)
+    .map(parseCommit);
+}
+
+function parseCommit(text) {
+  let [id, authorName, authorEmail, timestamp, message, ...rest] = text.split(
+    "\t"
+  );
+  let files = rest
+    .join("\t")
+    .trim()
+    .split("\n");
+
+  let patchSet = [];
+
+  for (let file of files) {
+    let [type, path] = file.split("\t");
+
+    patchSet.push({
+      path,
+      type: toSentryModType(type),
+    });
+  }
+
+  let payload = {
+    patch_set: patchSet,
+    respository: "npbee/sentry-releases-example",
+    author_name: authorName,
+    author_email: authorEmail,
+    timestamp,
+    message,
+    id,
+  };
+
+  return payload;
+}
+
+/**
+ * Sentry only accepts a few select 'modification' flags ('A', 'C', 'D'),
+ * but Git may return others
+ */
+function toSentryModType(gitType) {
+  if (gitType.startsWith("R") || gitType.startsWith("C")) {
+    return "M";
+  }
+
+  return gitType;
+}
+
+async function fetchLatestReleaseCommit() {
+  let releases = await callApi("releases/");
+  let releaseWithCommit = releases.find(release => release.lastCommit);
+
+  if (releaseWithCommit) {
+    let lastCommitHash = releaseWithCommit.lastCommit.id;
+    console.log("Found release with last commit hash: ", lastCommitHash);
+    return lastCommitHash;
+  }
+}
+
+async function createRelease(version, commits) {
+  let body = {
+    version,
+    projects: [process.env.SENTRY_PROJECT],
+    commits,
+  };
+
+  console.log("Creating release version: ", body.version);
+
+  return await callApi("releases/", {
+    method: "POST",
+    body: JSON.stringify(body),
+  });
+}
+
+async function callApi(path, ...options) {
+  return await fetch(
+    `https://sentry.io/api/0/organizations/npbee/${path}`,
+    Object.assign({}, options, {
+      headers: {
+        Authorization: `Bearer ${process.env.SENTRY_AUTH_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    })
+  ).then(response => {
+    if (!response.ok) {
+      console.error("Error calling Sentry API");
+      throw new Error(response.statusText);
+    }
+
+    return response.json();
+  });
+}
